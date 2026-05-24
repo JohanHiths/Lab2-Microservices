a1d8b0acc467c7018c016b4f49e65d3384ac9966
JohanHiths <johan.hietala@iths.se>
2026-05-25 00:08:52 +0200

    update

--- message-service/src/main/java/ai/messageservice/kafka/KafkaProducerService.java
+++ message-service/src/main/java/ai/messageservice/kafka/KafkaProducerService.java
@@ -5,7 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class KafkaProducerService {
@@ -13,8 +16,6 @@ public class KafkaProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final ObjectMapper objectMapper = new ObjectMapper();
     Logger logger = LoggerFactory.getLogger(KafkaProducerService.class);
-
-
 
     private static final String TOPIC = "chat-messages";
 
@@ -29,13 +30,29 @@ public class KafkaProducerService {
 
             String jsonMessage = objectMapper.writeValueAsString(entity);
 
+            CompletableFuture<SendResult<String, String>> future =
+                    kafkaTemplate.send(TOPIC, entity.getId(), jsonMessage);
 
-            kafkaTemplate.send(TOPIC, entity.getId(), jsonMessage);
+            future.whenComplete((result, ex) -> {
+                if (ex == null) {
 
-            logger.info("Message published!");
+                    long offset = result.getRecordMetadata().offset();
+                    int partition = result.getRecordMetadata().partition();
+                    System.out.println("🔥 [Kafka] Meddelande publicerat! Topic: " + TOPIC +
+                            " | Partition: " + partition +
+                            " | Offset: " + offset);
+                    logger.info("[Kafka] Meddelande publicerat! Topic: \" + TOPIC +\n" +
+                            " \" | Partition: \" + partition +\n" +
+                            " \" | Offset: \" + offset);");
+                } else {
+
+                    logger.error("❌ [Kafka] Kunde inte leverera meddelande till broker: " + ex.getMessage());
+                }
+            });
 
         } catch (Exception e) {
-            logger.error("Error publishing message: ❌ {}", e.getMessage());
+
+            logger.error("❌ [Kafka] Serialiseringsfel: \" + e.getMessage())");
         }
     }
 }
